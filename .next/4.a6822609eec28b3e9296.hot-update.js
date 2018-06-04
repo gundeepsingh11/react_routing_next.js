046eda2dc88e7afd306ce9ee9612bfe2d97db0d2
SAPIENT\gsood1 <gsood2@sapient.com>
2018-06-04 11:32:32 +0530

    setup for react routing with next.js

---
+++ .next/4.a6822609eec28b3e9296.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate(4,{
+
+/***/ "./node_modules/next/node_modules/webpack/buildin/harmony-module.js":
+false,
+
+/***/ "./pages/index.js":
+/***/ (function(module, __webpack_exports__) {
+
+"use strict";
+throw new Error("Module build failed: SyntaxError: D:\\Projects\\my-app\\pages\\index.js: Unterminated JSX contents (4:8)\n\n  2 |   <div>\n  3 |     <p>Hello Next.js\n> 4 |   </div>\n    |         ^\n  5 | )\n  6 | \n  7 | export default Index\n    at _class.raise (D:\\Projects\\my-app\\node_modules\\@babel\\core\\node_modules\\babylon\\lib\\index.js:778:15)\n    at _class.jsxReadToken (D:\\Projects\\my-app\\node_modules\\@babel\\core\\node_modules\\babylon\\lib\\index.js:7727:16)\n    at _class.readToken (D:\\Projects\\my-app\\node_modules\\@babel\\core\\node_modules\\babylon\\lib\\index.js:8095:21)\n    at _class.nextToken (D:\\Projects\\my-app\\node_modules\\@babel\\core\\node_modules\\babylon\\lib\\index.js:976:12)\n    at _class.next (D:\\Projects\\my-app\\node_modules\\@babel\\core\\node_modules\\babylon\\lib\\index.js:912:10)\n    at _class.eat (D:\\Projects\\my-app\\node_modules\\@babel\\core\\node_modules\\babylon\\lib\\index.js:917:12)\n    at _class.expect (D:\\Projects\\my-app\\node_modules\\@babel\\core\\node_modules\\babylon\\lib\\index.js:2051:10)\n    at _class.jsxParseClosingElementAt (D:\\Projects\\my-app\\node_modules\\@babel\\core\\node_modules\\babylon\\lib\\index.js:8001:12)\n    at _class.jsxParseElementAt (D:\\Projects\\my-app\\node_modules\\@babel\\core\\node_modules\\babylon\\lib\\index.js:8020:39)\n    at _class.jsxParseElementAt (D:\\Projects\\my-app\\node_modules\\@babel\\core\\node_modules\\babylon\\lib\\index.js:8024:34)\n    at _class.jsxParseElement (D:\\Projects\\my-app\\node_modules\\@babel\\core\\node_modules\\babylon\\lib\\index.js:8077:19)\n    at _class.parseExprAtom (D:\\Projects\\my-app\\node_modules\\@babel\\core\\node_modules\\babylon\\lib\\index.js:8084:21)\n    at _class.parseExprSubscripts (D:\\Projects\\my-app\\node_modules\\@babel\\core\\node_modules\\babylon\\lib\\index.js:2725:21)\n    at _class.parseMaybeUnary (D:\\Projects\\my-app\\node_modules\\@babel\\core\\node_modules\\babylon\\lib\\index.js:2704:21)\n    at _class.parseExprOps (D:\\Projects\\my-app\\node_modules\\@babel\\core\\node_modules\\babylon\\lib\\index.js:2616:21)\n    at _class.parseMaybeConditional (D:\\Projects\\my-app\\node_modules\\@babel\\core\\node_modules\\babylon\\lib\\index.js:2588:21)");
+
+/***/ })
+
+})
+//# sourceMappingURL=4.a6822609eec28b3e9296.hot-update.js.map
